c47cd5b97150c17d933cb8d6149a3f93a71cfee8
NaveedAslam205 <128507574+NaveedAslam205@users.noreply.github.com>
2025-07-11 23:20:33 +0500

    Update LLaMA model and finalize Render deploy setup

--- main.py
+++ main.py
@@ -4,14 +4,11 @@ from resume_analysis.ats_scoring import score_resume_against_jd
 from resume_analysis.lie_detection import detect_lies
 from resume_analysis.profile_extractor import extract_profile_info
 from resume_analysis.ai_analysis import generate_ai_feedback
-from resume_analysis.profile_extractor import extract_profile_info
 import os
 
 import fitz
 
 app = FastAPI()
-port = int(os.environ.get("PORT", 10000))
-uvicorn.run(app, host="0.0.0.0", port=port)
 
 app.add_middleware(
     CORSMiddleware,
